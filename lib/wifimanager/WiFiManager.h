4203cb93d95781c8a83092165b2d4ef2f6ee6201
ringbuchblock <ringbuchblock@virtualkitchen.org>
2016-04-11 19:56:38 +0200

    Revert "remove examples and 3rd party libs"
    This reverts commit f74a192f7d137524915b34641f686d3b7680c6a7.

---
+++ lib/wifimanager/WiFiManager.h
@@ -0,0 +1,65 @@
+/**
+ *
+ * @file WiFiManager.h
+ * @date 16.05.2015
+ * @author Markus Sattler
+ *
+ * Copyright (c) 2015 Markus Sattler. All rights reserved.
+ * This file is part of the esp8266 core for Arduino environment.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+#ifndef WIFIMANAGER_H_
+#define WIFIMANAGER_H_
+
+#include "ESP8266WiFi.h"
+#undef min
+#undef max
+#include <vector>
+
+// #define DEBUG_WIFI_MULTI(...) Serial1.printf( __VA_ARGS__ )
+
+#ifndef DEBUG_WIFI_MULTI
+#define DEBUG_WIFI_MULTI(...)
+#endif
+
+typedef struct {
+        char * ssid;
+        char * passphrase;
+} WifiAPlist_t;
+
+class WiFiManager {
+    public:
+        WiFiManager();
+        ~WiFiManager();
+
+        void loop();
+        
+        void setAPlist(std::vector<WifiAPlist_t> APlist);
+        std::vector<WifiAPlist_t> getAPlist();
+
+        wl_status_t AutoConnect(void);
+        wl_status_t FallbackAP(void);
+
+
+    private:
+        void APlistClean(void);
+        std::vector<WifiAPlist_t> APlist;
+};
+
+#endif /* WIFIMANAGER_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * ESP Boilerplate OS - main.cpp
+ * (c) by Christoph (doebi) Döberl
+ *
+ */
+#include "ESPThing.h"
+
+bool ping = false;
+String ping_msg = "";
+
+void pong_loop(String * msg) {
+    if (ping) {
+        ping = false;
+        *msg = ping_msg;
+    }
+}
+
+void heartbeat_loop(String * msg) {
+    *msg = String(millis());
+}
+
+void ping_cb(const MQTT::Publish& pub) {
+    ping_msg = pub.payload_string();
+    ping = true;
+}
+
+void setup() {
+    Serial.begin(115200);
+    Thing.addOutput(Output("pong", pong_loop));
+    Thing.addOutput(Output("heartbeat", heartbeat_loop, 1000 * 60 * 3));
+    Thing.addInput(Input("ping", ping_cb));
+}
+
+void loop() {
+    Thing.loop();
+}
